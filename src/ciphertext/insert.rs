1d4f426ee5051c3e1700e0a3b25ad4c9ee477b90
Matthias Geihs <matthias@geihs.de>
2023-11-15 09:09:52 +0100

    wip improve insert

--- src/ciphertext/insert.rs
+++ src/ciphertext/insert.rs
@@ -1,6 +1,6 @@
 use tfhe::integer::RadixCiphertext;
 
-use rayon::prelude::*;
+use rayon::{join, prelude::*};
 
 use crate::{
     ciphertext::{binary_if_then_else, FheAsciiChar, Uint},
@@ -55,6 +55,57 @@ impl FheString {
         let l = std::cmp::min(a.max_len() + b.max_len(), Self::max_len_with_key(k));
         let b_len = b.len(k);
 
+        // (i < index + b.len, (b[i - index], a[i - b.len])
+        let (i_lt_index_add_blen, (b_at_i_sub_index, a_at_i_sub_blen)) = join(
+            || {
+                (0..l)
+                    .into_par_iter()
+                    .map(|i| {
+                        let index_add_blen = k.k.add_parallelized(index, &b_len);
+                        k.k.scalar_gt_parallelized(&index_add_blen, i as Uint)
+                    })
+                    .collect::<Vec<_>>()
+            },
+            || {
+                let (b_at_i_sub_index, a_at_i_sub_blen) = join(
+                    || {
+                        (0..l)
+                            .into_par_iter()
+                            .map(|i| {
+                                let i_radix = k.create_value(i as Uint);
+                                let i_sub_index = k.k.sub_parallelized(&i_radix, &index);
+                                b.char_at(k, &i_sub_index)
+                            })
+                            .collect::<Vec<_>>()
+                    },
+                    || {
+                        (0..l)
+                            .into_par_iter()
+                            .map(|i| {
+                                let i_radix = k.create_value(i as Uint);
+                                let i_sub_b_len = k.k.sub_parallelized(&i_radix, &b_len);
+                                a.char_at(k, &i_sub_b_len)
+                            })
+                            .collect::<Vec<_>>()
+                    },
+                );
+                (b_at_i_sub_index, a_at_i_sub_blen)
+            },
+        );
+
+        // c2[i] = (i < index + b.len ? b[i - index] : a[i - b.len])
+        let c2 = (0..l)
+            .into_par_iter()
+            .map(|i| {
+                binary_if_then_else(
+                    k,
+                    &i_lt_index_add_blen[i],
+                    &b_at_i_sub_index[i].0,
+                    &a_at_i_sub_blen[i].0,
+                )
+            })
+            .collect::<Vec<_>>();
+
         let mut v = (0..l)
             .into_par_iter()
             .map(|i| {
@@ -66,27 +117,8 @@ impl FheString {
                 // c1 = a[i]
                 let c1 = &a.0[i % a.0.len()].0;
 
-                // b[i - index]
-                let i_radix = k.create_value(i as Uint);
-                let i_sub_index = k.k.sub_parallelized(&i_radix, &index);
-                let b_i_sub_index = b.char_at(k, &i_sub_index);
-
-                // a[i - b.len]
-                let i_sub_b_len = k.k.sub_parallelized(&i_radix, &b_len);
-                let a_i_sub_b_len = a.char_at(k, &i_sub_b_len);
-
-                // c2 = i < index + b.len ? b[i - index] : a[i - b.len]
-                let index_add_blen = k.k.add_parallelized(index, &b_len);
-                let i_lt_index_add_blen = k.k.scalar_gt_parallelized(&index_add_blen, i as Uint);
-                let c2 = binary_if_then_else(
-                    k,
-                    &i_lt_index_add_blen,
-                    &b_i_sub_index.0,
-                    &a_i_sub_b_len.0,
-                );
-
                 // c = c0 ? c1 : c2
-                let c = binary_if_then_else(k, &c0, c1, &c2);
+                let c = binary_if_then_else(k, &c0, c1, &c2[i]);
                 FheAsciiChar(c)
             })
             .collect::<Vec<_>>();
